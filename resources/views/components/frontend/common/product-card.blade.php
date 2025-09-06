91cf22dd669ed51928d61aac35e9390bb50b59d8
Mohamed Shakeel <shakeel.mails2@gmail.com>
2025-09-06 14:59:23 +0530

    Convert the product-card and product-section to tailwind

--- resources/views/components/frontend/common/product-card.blade.php
+++ resources/views/components/frontend/common/product-card.blade.php
@@ -1,20 +1,20 @@
 <div class="shop-block-one">
-    <div class="inner-box">
-        <div class="image-box">
-            <figure class="image">
-                <a href="{!! $link !!}"><img src="{{ $image }}" alt="{{ $alt }}" /></a>
+    <div class="inner-box group relative block px-[30px] pt-[30px] pb-0 text-center transition-all duration-500 ease before:absolute before:border-r before:border-dashed before:border-gray-300 before:top-0 before:right-[-15px] before:h-full before:content-['']">
+        <div class="image-box relative block">
+            <figure class="image relative block max-w-[235px] w-full mx-auto overflow-hidden">
+                <a href="{!! $link !!}"><img class="w-full transition-all duration-500 ease hover:scale-105" src="{{ $image }}" alt="{{ $alt }}" /></a>
             </figure>
         </div>
-        <div class="lower-content" style="height: 150px">
-            <span class="text">{{ $category }}</span>
-            <h4>
-                <a class="text-[16px] !h-[50px] line-clamp-2" href="{!! $link !!}">{!! $name !!}</a>
+        <div class="lower-content relative block bg-white pt-[35px] pb-4 transition-all duration-500 ease group-hover:pb-[90px] group-hover:-mt-[73px] h-[150px]">
+            <span class="text relative block text-sm leading-5 mb-1 text-gray-500">{{ $category }}</span>
+            <h4 class="block text-xl leading-6 mb-1">
+                <a class="inline-block text-[var(--title-color)] text-base h-[50px] overflow-hidden line-clamp-2" href="{!! $link !!}">{!! $name !!}</a>
             </h4>
-            <div class="price">
+            <div class="price relative block text-xl text-red-600 font-bold mb-[17px]">
                 @if (!empty($originalPrice) && floatval($originalPrice) > floatval($price))
-                    <del class="!text-[14px]">{{ env('DEFAULT_CURRENCY') . ' ' . number_format((float) $originalPrice, 2) }}</del>
+                    <del class="text-base leading-[28px] text-gray-600 font-normal mr-[10px] text-sm">{{ env('DEFAULT_CURRENCY') . ' ' . number_format((float) $originalPrice, 2) }}</del>
                 @endif
-                <span class="text-[14px]">
+                <span class="text-sm">
                     {{ env('DEFAULT_CURRENCY') . ' ' . number_format((float) $price, 2) }}
                 </span>
             </div>
--- resources/views/components/frontend/common/product-section.blade.php
+++ resources/views/components/frontend/common/product-section.blade.php
@@ -1,114 +1,12 @@
-<style>
-/* shop-style-nine styles */
-.shop-style-nine {
-    position: relative;
-}
-
-.shop-style-nine .title-box {
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-}
-
-/* project-tab styles */
-.project-tab .p-tab {
-    position: absolute;
-    left: 0px;
-    top: 0px;
-    width: 100%;
-    height: auto;
-    opacity: 0;
-    visibility: hidden;
-}
-
-.project-tab .p-tab.active-tab {
-    position: relative;
-    visibility: visible;
-    opacity: 1;
-    z-index: 5;
-}
-
-.project-tab .p-tabs-content {
-    position: relative;
-    display: block;
-}
-
-.project-tab .p-tab.active-tab .shop-block-one,
-.project-tab .p-tab.active-tab .collection-block-one {
-    -webkit-transform: scaleX(1);
-    -ms-transform: scaleX(1);
-    -o-transform: scaleX(1);
-    -moz-transform: scaleX(1);
-    transform: scaleX(1);
-    -webkit-transition: all 700ms ease;
-    -ms-transition: all 700ms ease;
-    -o-transition: all 700ms ease;
-    transition: all 700ms ease;
-}
-
-.project-tab .p-tab .shop-block-one,
-.project-tab .p-tab .collection-block-one {
-    position: relative;
-    -webkit-transform: scaleX(0);
-    -ms-transform: scaleX(0);
-    -o-transform: scaleX(0);
-    -moz-transform: scaleX(0);
-    transform: scaleX(0);
-}
-
-/* large-container styles */
-.large-container {
-    max-width: 1800px;
-    padding: 0px 15px;
-    margin: 0 auto;
-}
-
-/* sec-title styles */
-.sec-title {
-    position: relative;
-    display: block;
-    z-index: 1;
-}
-
-.sec-title h2 {
-    position: relative;
-    display: block;
-    font-size: 48px;
-    line-height: 54px;
-}
-
-.sec-title h2 span {
-    /* Empty as per original */
-}
-
-.sec-title.light h2 {
-    color: #fff;
-}
-
-/* margin and padding utilities */
-.mb_60 {
-    margin-bottom: 60px;
-}
-
-.pt_70 {
-    padding-top: 70px;
-}
-
-.pb_90 {
-    padding-bottom: 90px;
-}
-</style>
-
-<section class="shop-style-nine {{ $class ?? 'pt_70 pb_90' }}">
-    <div class="large-container">
+<section class="shop-style-nine relative {{ $class ?? 'pt-[70px] pb-[90px]' }}">
+    <div class="large-container max-w-[1800px] px-4 mx-auto">
         <div class="project-tab">
-            <div class="title-box mb_60">
-                <div class="sec-title">
-                    <h2>{{ $title ?? "" }}</h2>
+            <div class="title-box flex items-center justify-between mb-[60px]">
+                <div class="sec-title relative block z-[1]">
+                    <h2 class="relative block text-[48px] leading-[54px]">{{ $title ?? "" }}</h2>
                 </div>
             </div>
-            <div class="p-tabs-content inner-container">
+            <div class="p-tabs-content inner-container relative block">
                 {{ $slot }}
             </div>
         </div>
@@ -124,7 +22,6 @@ document.addEventListener('DOMContentLoaded', function() {
             button.addEventListener('click', function(e) {
                 e.preventDefault();
                 const target = document.querySelector(this.getAttribute('data-tab'));
-
                 if (target && target.classList.contains('active-tab')) {
                     return false;
                 } else {
@@ -134,7 +31,6 @@ document.addEventListener('DOMContentLoaded', function() {
                     });
                     // Add active class to clicked button
                     this.classList.add('active-btn');
-
                     // Remove active class from all tabs
                     document.querySelectorAll('.project-tab .p-tabs-content .p-tab').forEach(tab => {
                         tab.classList.remove('active-tab');
@@ -149,5 +45,4 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 </script>
-
 <!-- shop-style-nine end -->
