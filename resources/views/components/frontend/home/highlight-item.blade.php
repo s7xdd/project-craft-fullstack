474c28c02fe03485d9b1e3bca033a65cb23f2442
Mohamed Shakeel <shakeel.mails2@gmail.com>
2025-09-07 02:00:50 +0530

    Edit highlights

--- resources/views/components/frontend/home/highlight-item.blade.php
+++ resources/views/components/frontend/home/highlight-item.blade.php
@@ -1,8 +1,19 @@
-<li>
-    <div class="single-item">
-        <div class="icon-box">
-            <img src="{{ $icon }}" alt="" style="height: 43px;" />
-        </div>
-        <h5>{{ $text }}</h5>
+<section class="highlights-section pt_40">
+  <div class="large-container">
+    <div class="inner-container">
+      <!-- Inline flex wrap -->
+      <ul class="list-item clearfix" style="display:flex; flex-wrap:wrap; margin-left:-8px; margin-right:-8px;">
+        <li style="width:50%; padding-left:8px; padding-right:8px;">
+          <div class="single-item flex flex-col alighn-center">
+            <div class="icon-box">
+              <img src="{{ $icon }}" alt="" style="height: 43px;" />
+            </div>
+            <div class="text-xs">{{ $text }}</div>
+          </div>
+        </li>
+        <!-- repeat li... -->
+        {{ $slot }}
+      </ul>
     </div>
-</li>
+  </div>
+</section>
--- resources/views/components/frontend/home/highlights.blade.php
+++ resources/views/components/frontend/home/highlights.blade.php
@@ -1,10 +1,10 @@
 <section class="highlights-section pt_40">
-    <div class="large-container">
-        <div class="inner-container">
-            <ul class="list-item clearfix">
-                {{ $slot }}
-            </ul>
-        </div>
+  <div class="large-container">
+    <div class="inner-container">
+      <!-- Inline grid: two columns + gap -->
+      <ul class="list-item clearfix" style="display:grid; grid-template-columns:repeat(2, minmax(0, 1fr)); gap:12px;">
+        {{ $slot }}
+      </ul>
     </div>
+  </div>
 </section>
-<!-- highlights-section end -->
