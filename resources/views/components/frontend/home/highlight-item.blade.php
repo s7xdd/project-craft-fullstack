ee6affc9d98c0d6f5c4255e13ac48877e57bcc55
Mohamed Shakeel <shakeel.mails2@gmail.com>
2025-10-07 20:32:34 +0530

    Highlight fixed

--- resources/views/components/frontend/home/highlight-item.blade.php
+++ resources/views/components/frontend/home/highlight-item.blade.php
@@ -1,7 +1,7 @@
 
-    <div class="flex flex-col sm:flex-row items-center gap-4 mb-5 xl:mb-0">
-        <div class="inline-block w-15 h-15 leading-15 text-center bg-white rounded-full border border-gray-300 flex items-center justify-center">
+    <div class="flex flex-col sm:flex-row items-center justify-center gap-4 xl:mb-0">
+        <div class="inline-block w-15 h-15 leading-5 text-center bg-white rounded-full flex items-center justify-center">
             <img src="{{ $icon }}" alt="" class="h-11" />
         </div>
-        <h5 class="text-lg leading-6 text-center sm:text-left">{{ $text }}</h5>
+        <div class="text-sm md:text-md lg:text-lg leading-6 text-center sm:text-left">{{ $text }}</div>
     </div>
--- resources/views/components/frontend/home/highlights.blade.php
+++ resources/views/components/frontend/home/highlights.blade.php
@@ -1,7 +1,7 @@
-<section class="relative z-2 w-full py-10 px-5">
+<section class="relative z-2 w-full px-5">
     <div class="mx-auto">
-        <div class="bg-white rounded-2xl border border-gray-300 p-4 sm:p-8 sm:px-12 pb-5">
-            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        <div class="bg-white rounded-[30px] border border-gray-300 p-2 sm:p-8 sm:px-12 ">
+            <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
                 {{ $slot }}
             </div>
         </div>
