d7243c535704684512872903aa445a06505731f4
Mohamed Shakeel <shakeel.mails2@gmail.com>
2025-10-25 21:46:26 +0530

    Product detail page layout order issue fix

--- resources/views/components/frontend/product-detail/product-gallery.blade.php
+++ resources/views/components/frontend/product-detail/product-gallery.blade.php
@@ -1,11 +1,11 @@
-  <section class="py-16 lg:py-24">
+  <section class="py-16 lg:pb-24">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="mb-16">
               <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12">
-                  <div class="order-1 lg:order-2">
+                  <div class="order-1 lg:order-1">
                       {{ $slot }}
                   </div>
-                  <div class="order-2 lg:order-1">
+                  <div class="order-2 lg:order-2">
                       {{ $productDetails }}
                   </div>
               </div>
