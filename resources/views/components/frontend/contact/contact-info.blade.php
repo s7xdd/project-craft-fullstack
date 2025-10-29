7322c5565fe4300099c5374b0e0feaabefc6d7e5
s7xdd <kmshamil123@gmail.com>
2025-10-29 22:27:13 +0400

    add

--- resources/views/components/frontend/contact/contact-info.blade.php
+++ resources/views/components/frontend/contact/contact-info.blade.php
@@ -22,7 +22,7 @@
         </div>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
             @foreach($contacts as $index => $contact)
-            <div class="bg-{{ ['orange', 'blue', 'green', 'red'][$index % 4] }}-200 rounded-2xl p-8 transition-all duration-500 hover:shadow-xl hover:-translate-y-2">
+            <div class=" rounded-2xl p-8 transition-all duration-500 hover:shadow-xl hover:-translate-y-2">
                 <div class="w-20 h-20 bg-white rounded-full flex items-center justify-center mx-auto mb-4">
                     <i class="fas fa-{{ ['map-marker-alt', 'envelope', 'phone-alt', 'question'][$index % 4] }} text-2xl text-gray-700"></i>
                 </div>
